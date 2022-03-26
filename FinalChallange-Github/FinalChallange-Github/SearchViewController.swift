8720c47fd4cee145aa085f2319f00a1fe0f56309
BrunoOS <brunno.os159@gmail.com>
2022-03-26 16:23:13 -0300

    Project Setup

---
+++ FinalChallange-Github/FinalChallange-Github/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalChallange-Github
+//
+//  Created by Aloc FL00030 on 26/03/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
