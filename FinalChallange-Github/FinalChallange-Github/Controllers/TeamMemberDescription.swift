206c3ffaf11106ef9113f0885ee347abdc3871e5
BrunoOS <brunno.os159@gmail.com>
2022-03-27 16:31:05 -0300

    Revert "Merge pull request #3 from Brunnoos/feature/team_members"
    This reverts commit 39bf0925c6bfc984d1c99a2705eb52845c4b02e7, reversing
    changes made to 20f22a2b1b76e0ff308940b5b87261f60fea9770.

--- FinalChallange-Github/FinalChallange-Github/Controllers/TeamMemberDescription.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TeamMemberDescription.swift
-//  FinalChallange-Github
-//
-//  Created by Idwall Go Dev 006 on 27/03/22.
-//
-
-import Foundation
--- FinalChallange-Github/FinalChallange-Github/Storyboards/TeamMembersView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TeamMembersView.swift
-//  FinalChallange-Github
-//
-//  Created by Idwall Go Dev 006 on 26/03/22.
-//
-
-import Foundation
