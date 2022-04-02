83404d73dbf6194aaae64c9177d9667c576ca06b
BrunoOS <brunno.os159@gmail.com>
2022-04-02 09:12:42 -0300

    WIP Home Screen with TableView

--- FinalChallange-Github/FinalChallange-Github/Controllers/Main Screens/SearchViewController.swift
+++ FinalChallange-Github/FinalChallange-Github/Controllers/Main Screens/SearchViewController.swift
@@ -9,17 +9,63 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
-    private lazy var repositories = [Repository]() {
+    // MARK: - Fileprivate Properties
+    
+    fileprivate enum ViewState {
+        case loading
+        case normal
+        case error
+        case zeroResults
+    }
+    
+    fileprivate var state: ViewState = .normal {
         didSet {
-            if repositories.count > 0 {
-                self.openRepoDetail(repository: repositories[0])
+            setupView()
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var repositories: [Repository]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
         }
     }
     
+    // MARK: - Lazy Properties
+    
+    private lazy var loadingIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.style = .large
+        indicator.color = .orange
+        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        /// Register Cell nIb
+        tableView.register(RepositoryTableViewCell.self, forCellReuseIdentifier: RepositoryTableViewCell.identifier)
+        
+        return tableView
+    }()
+    
+    
+    
+    // MARK: - View Controller Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        state = . loading
+        setupTableViewLayout()
         
         // REMOVE THIS AFTER THIS SCREENN IS UPDATED
         SearchAPI.shared.searchLanguage(searchParams: SearchParameters(language: "swift")) { response in
@@ -27,12 +73,74 @@ class SearchViewController: UIViewController {
             switch response {
             case .success(let result):
                 self.repositories = result.repositories
+                self.state = .normal
             case .failure(let error):
                 print(error)
+                self.state = .error
             }
         }
     }
+    
+    // MARK: - Setup View State
+    
+    private func setupView() {
+        switch state {
+        case .loading:
+            onLoadingState()
+        case .normal:
+            onNormalState()
+        case .zeroResults:
+            onZeroResultsState()
+        case .error:
+            onErrorState()
+        }
+    }
+    
+    private func onLoadingState() {
+        DispatchQueue.main.async {
+            self.loadingIndicator.startAnimating()
+            self.tableView.isHidden = true
+            
+        }
+    }
+    
+    private func onNormalState() {
+        DispatchQueue.main.async {
+            self.loadingIndicator.stopAnimating()
+            self.tableView.isHidden = false
+            
+        }
+    }
+    
+    private func onZeroResultsState() {
+        DispatchQueue.main.async {
+            self.loadingIndicator.stopAnimating()
+            self.tableView.isHidden = true
+        }
+    }
+    
+    private func onErrorState() {
+        DispatchQueue.main.async {
+            self.loadingIndicator.stopAnimating()
+            self.tableView.isHidden = true
+        }
+    }
+    
+    // MARK: - Table View Setup
+    
+    private func setupTableViewLayout() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 
+    // MARK: - Open Repository Detail Methods
+    
     private func openRepoDetail(repository: Repository) {
         let detailsView = RepoDetailsViewController()
         
@@ -42,3 +150,37 @@ class SearchViewController: UIViewController {
     }
 }
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let repositories = self.repositories {
+            openRepoDetail(repository: repositories[indexPath.row])
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 95
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let repositories = self.repositories {
+            return repositories.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryTableViewCell.identifier, for: indexPath) as? RepositoryTableViewCell else { return UITableViewCell() }
+        
+        if let repositories = self.repositories {
+            let repository = repositories[indexPath.row]
+            cell.setupCell(repository: repository, isFavorite: false)
+        }
+        
+        return cell
+    }
+    
+    
+}
--- FinalChallange-Github/FinalChallange-Github/Utilities/RepositoryTableViewCell.swift
+++ FinalChallange-Github/FinalChallange-Github/Utilities/RepositoryTableViewCell.swift
@@ -6,31 +6,135 @@
 //
 
 import UIKit
+import Kingfisher
 
 class RepositoryTableViewCell: UITableViewCell {
+    
+    // MARK: - Static Properties
+    
+    static let identifier = "RepositoryTableViewCell"
     
     // MARK: - Lazy Properties
     
     lazy var repoImageView: UIImageView = {
         let image = UIImageView()
-        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 85 / 2
         return image
     }()
     
     lazy var repoNameLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16)
         return label
     }()
     
     lazy var repoDescriptionLabel: UILabel = {
         let label = UILabel()
-        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 3
+        label.font = UIFont.systemFont(ofSize: 14)
         return label
     }()
     
     lazy var repoFavoriteButton: UIButton = {
         let button = UIButton()
-        
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    // MARK: - Cell Setup
+    
+    func setupCell(repository: Repository, isFavorite: Bool) {
+        setupLayouts()
+        
+        setupRepoName(name: repository.name)
+        setupRepoImage(imageURL: repository.owner.avatarURL)
+        setupRepoDescription(description: repository.itemDescription)
+        setupFavoriteButton(isFavorite: isFavorite)
+    }
+    
+    private func setupRepoName(name: String) {
+        repoNameLabel.text = name
+    }
+    
+    private func setupRepoImage(imageURL: String) {
+        
+        if let url = URL(string: imageURL) {
+            repoImageView.kf.setImage(with: url)
+        }
+    }
+    
+    private func setupRepoDescription(description: String?) {
+        if let description = description {
+            repoDescriptionLabel.isHidden = false
+            repoDescriptionLabel.text = description
+        } else {
+            repoDescriptionLabel.isHidden = true
+        }
+    }
+    
+    private func setupFavoriteButton(isFavorite: Bool) {
+        if isFavorite {
+            repoFavoriteButton.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+        } else {
+            repoFavoriteButton.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+        }
+    }
+    
+    // MARK: - Layout Setup
+    
+    private func setupLayouts() {
+        setupImageLayout()
+        setupFavoriteButtonLayout()
+        setupNameLayout()
+        setupDescriptionLayout()
+    }
+    
+    private func setupImageLayout() {
+        addSubview(repoImageView)
+        
+        NSLayoutConstraint.activate([
+            repoImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            repoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            repoImageView.heightAnchor.constraint(equalToConstant: 85),
+            repoImageView.widthAnchor.constraint(equalToConstant: 85)
+        ])
+    }
+    
+    private func setupNameLayout() {
+        addSubview(repoNameLabel)
+        
+        NSLayoutConstraint.activate([
+            repoNameLabel.topAnchor.constraint(equalTo: repoImageView.topAnchor),
+            repoNameLabel.leadingAnchor.constraint(equalTo: repoImageView.trailingAnchor, constant:  13),
+            repoNameLabel.trailingAnchor.constraint(equalTo: repoFavoriteButton.leadingAnchor, constant: -4),
+            repoNameLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+    }
+    
+    private func setupDescriptionLayout() {
+        addSubview(repoDescriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            repoDescriptionLabel.topAnchor.constraint(equalTo: repoNameLabel.bottomAnchor),
+            repoDescriptionLabel.leadingAnchor.constraint(equalTo: repoNameLabel.leadingAnchor),
+            repoDescriptionLabel.trailingAnchor.constraint(equalTo: repoNameLabel.trailingAnchor),
+            repoDescriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func setupFavoriteButtonLayout() {
+        addSubview(repoFavoriteButton)
+        
+        NSLayoutConstraint.activate([
+            repoFavoriteButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -6),
+            repoFavoriteButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            repoFavoriteButton.heightAnchor.constraint(equalToConstant: 36),
+            repoFavoriteButton.widthAnchor.constraint(equalToConstant: 36)
+        ])
+    }
+    
 }
